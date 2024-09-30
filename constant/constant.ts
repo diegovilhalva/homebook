68e6ff9c66cb3cb2708d66b72530534edf4abcbb
Diego Vilhalva <diegorocha450@yahoo.com>
2024-09-30 11:46:42 -0300

    navbar created

---
+++ constant/constant.ts
@@ -0,0 +1,32 @@
+export const navLinks = [
+    {
+      id: 1,
+      url: "#",
+      label: "Início",
+    },
+    {
+      id: 2,
+      url: "#",
+      label: "Sobre",
+    },
+    {
+      id: 3,
+      url: "#",
+      label: "Cursos",
+    },
+    {
+      id: 4,
+      url: "#",
+      label: "Depoimentos",
+    },
+    {
+      id: 5,
+      url: "#",
+      label: "Blog",
+    },
+    {
+      id: 6,
+      url: "#",
+      label: "Contato",
+    },
+  ];
