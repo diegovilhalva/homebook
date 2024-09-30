819ae05054046d2b10e71e085bc01a9ffe4e164a
Diego Vilhalva <diegorocha450@yahoo.com>
2024-09-30 14:04:37 -0300

    hero section created

---
+++ app/components/Home/Hero/HeroImage.tsx
@@ -0,0 +1,17 @@
+"use client"
+import Image from 'next/image'
+import React from 'react'
+import Tilt from 'react-parallax-tilt'
+
+
+const HeroImage = () => {
+  return (
+    <Tilt>
+        <div className="hidden lg:block">
+            <Image src="/images/hero.png" width={800} height={600} alt='hero image' />
+        </div>
+    </Tilt>
+  )
+}
+
+export default HeroImage
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import {Space_Grotesk} from "next/font/google"
 import "./globals.css";
-import ResponsiveNav from "./components/Navbar/ResponsiveNav";
+import ResponsiveNav from "./components/Home/Navbar/ResponsiveNav";
 
 const font = Space_Grotesk({
   weight:["300","400","500","600","700"],
