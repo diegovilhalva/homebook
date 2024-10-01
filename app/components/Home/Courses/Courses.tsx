b1f5e5dd12bc337f014522be0a83446d1885afa4
Diego Vilhalva <diegorocha450@yahoo.com>
2024-10-01 12:24:24 -0300

    courses section created

---
+++ app/components/Home/Courses/Courses.tsx
@@ -0,0 +1,26 @@
+import { coursesData } from '@/app/data/data'
+import Image from 'next/image'
+import React from 'react'
+import CourseCard from './CourseCard'
+
+const Courses = () => {
+    return (
+        <div className="pt-16 pb-12 relative bg-gray-200">
+            <Image src="/images/cb.png" alt="image" width={800} height={800} className='absolute top-[30%]
+        animate-bounce' />
+
+            <div className="w-[80%] pt-8 pb-8 mx-auto">
+                <h1 className='text-4xl md:text-5xl text-gray-900 font-bold'>Cursos Populares</h1>
+                <div className="md:mt-16 mt-10 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+                    {coursesData.map((course) => (
+                        <div className="" key={course.id}>
+                            <CourseCard course={course} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Courses
